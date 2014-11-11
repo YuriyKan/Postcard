af85d5205d87b70d59651ea34d1fd32d2076d45d
Yuriy Kan <yu.v.kan.90@gmail.com>
2014-11-11 15:20:00 +0500

    name label functionality

--- Lesson1st/ViewController.swift
+++ Lesson1st/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     
@@ -30,10 +32,19 @@ class ViewController: UIViewController {
     @IBAction func sendMailButtonPressed(sender: AnyObject) {
         // adding a comments here to test the commits
         messageLabel.hidden = false
-        messageLabel.text = "Hello World"
         messageLabel.text = enterMessageTextField.text
+        messageLabel.textColor = UIColor.redColor()
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
+        
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
         
     }
